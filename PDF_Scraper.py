fc8d3494784a1f2f8f49fa8ed7c535aaeb81fc32
rmsmith251 <61593418+rmsmith251@users.noreply.github.com>
2020-04-29 11:15:22 -0500

    Update PDF_Scraper.py

--- PDF_Scraper.py
+++ PDF_Scraper.py
@@ -14,6 +14,7 @@ import os
 root = Tk()
 root.withdraw()
 
+# Popup to ask where to save the CSV and what to call the CSV
 file_save = filedialog.asksaveasfilename(defaultextension='.csv')
 
 # print(file_save)
@@ -26,16 +27,20 @@ with open(file_save, 'w', newline='') as csvfile:
 
     pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
     pages = pdfReader.numPages
+    
+    # For debugging
     print(pdfReader.numPages)
 
+    # can obviously be whatever you want. This was applicable to my situation.
     fieldnames = ['Testpack', 'PDF_Data']
     CSVOutput = csv.DictWriter(csvfile, fieldnames=fieldnames)
 
+    # Scrub from each page in the range and output to new line in CSV
     for x in range(pages):
         pageObj = pdfReader.getPage(x)
 
         print(pdfReader.resolvedObjects)
-
+        
         CSVOutput.writerow({'Testpack': pdfFileObj, 'PDF_Data': pageObj.extractText()})
 
     # pageObj = pdfReader.getPage(0)
